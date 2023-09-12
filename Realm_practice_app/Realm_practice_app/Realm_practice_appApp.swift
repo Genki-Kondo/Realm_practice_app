9eba1372eef37cb870a025aba436a3e7f95c96c4
近藤元気 <genki7onepiece@gmail.com>
2023-09-12 22:52:05 +0900

    初めてのコミット

---
+++ Realm_practice_app/Realm_practice_app/Realm_practice_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Realm_practice_appApp.swift
+//  Realm_practice_app
+//
+//  Created by 近藤元気 on 2023/09/12.
+//
+
+import SwiftUI
+
+@main
+struct Realm_practice_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
