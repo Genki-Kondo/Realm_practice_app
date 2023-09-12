2e1c4f709733c11ae3c8d2f2b615918fb3956287
近藤元気 <genki7onepiece@gmail.com>
2023-09-12 23:34:21 +0900

    cocoapod でrealmをインポート

---
+++ Realm_practice_app/Pods/Target Support Files/Pods-Realm_practice_app/Pods-Realm_practice_app-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Realm_practice_app : NSObject
+@end
+@implementation PodsDummy_Pods_Realm_practice_app
+@end
--- Realm_practice_app/Realm_practice_app/Realm_practice_appApp.swift
+++ Realm_practice_app/Realm_practice_app/Realm_practice_appApp.swift
@@ -4,7 +4,7 @@
 //
 //  Created by 近藤元気 on 2023/09/12.
 //
-
+//https://qiita.com/tanakadaichi_1989/items/c5c20e9fdb37649579e1を参考にした練習用アプリ
 import SwiftUI
 
 @main
